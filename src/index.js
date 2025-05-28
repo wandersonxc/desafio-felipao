ff40a2853f9ab9c5b236be65c2bb2df417a52701
wandersonxc <wandersonxcarvalho@gmail.com>
2025-05-28 17:42:45 -0300

    projeto classificador de nível de heróis - finalizado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-let nome = "Wanderson";
-console.log(nome)
+//variáveis
+let nomeHeroi = "Super Wanderson"
+let xpHeroi = 11000
+
+//estrutura de decisão para classificar o nível
+let nivelHeroi
+
+if (xpHeroi < 1000) {
+  nivelHeroi = "Ferro"  
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+  nivelHeroi = "Bronze"
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+  nivelHeroi = "Prata"
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+  nivelHeroi = "Ouro"
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+  nivelHeroi = "Platina"
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+  nivelHeroi = "Ascendente"
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+  nivelHeroi = "Imortal"
+} else if (xpHeroi >= 10001) {
+  nivelHeroi = "Radiante"
+}
+
+//mensagem final
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi + "!");
